542012b2064c8325ab774e2b8b0971599a7186b4
MaorgD <111678012+MaorgD@users.noreply.github.com>
2022-09-05 15:04:32 +0300

    Add files via upload

---
+++ js_todo/app_todo.js
@@ -0,0 +1,64 @@
+const tasks_ar = [];
+
+window.onload = function(){
+    declareEvents();
+    checkLocal();
+}
+
+const checkLocal = function(){
+if(localStorage["tasks"]){
+    let _ar = JSON.parse(localStorage["tasks"]);
+    console.log(_ar);
+_ar.forEach(function(item){
+    tasks_ar.push(item);
+})
+createTasks(tasks_ar);
+}
+}
+
+const declareEvents = function () {
+    let add_btn = document.querySelector("#add_btn");
+    let reset_btn = document.querySelector("#reset_btn");
+
+    reset_btn.addEventListener("click",function(){
+        tasks_ar.splice(0,tasks_ar.length);
+        createTasks(tasks_ar);
+    })
+
+    add_btn.addEventListener("click",function(){
+       let name_val = document.querySelector("#id_name").value;
+       let time_val = document.querySelector("#id_time").value;
+       let task_obj = {
+        name:name_val,
+        time:time_val,
+        id:Date.now()
+       }
+       tasks_ar.push(task_obj);
+       console.log(tasks_ar);
+    //    console.log(task_obj);
+    createTasks(tasks_ar);
+    })
+
+}
+
+const delsingleTask = function(_idDel){
+    tasks_ar.forEach(function(item,i){
+        if(item.id == _idDel){
+            tasks_ar.splice(i,1);
+        }
+    })
+    createTasks(tasks_ar); 
+}
+
+
+const createTasks = function(_ar){
+    document.querySelector("#id_parent").innerHTML = ""
+    _ar = _.sortBy(_ar,"time");
+// לשמור מערך בלוקאל
+localStorage.setItem("tasks",JSON.stringify(_ar));
+    _ar.forEach(function(item){
+        let task = new Task("#id_parent",item);
+        task.render();
+        
+    });
+}
---
+++ js_todo/taskClass.js
@@ -0,0 +1,25 @@
+class Task{
+    constructor(_parent,_item){
+        this.parent = _parent;
+        this.name = _item.name;
+        this.time = _item.time;
+        this.id = _item.id;
+    }
+
+    render(){
+        let div = document.createElement("div");
+        div.className = "shadow-sm my-2 p-2";
+        document.querySelector(this.parent).append(div);
+
+        div.innerHTML += `
+        <button class="btn btn-danger float-end">x</button>
+        <h4>${this.name} - ${this.time}</h4>
+        
+        `
+
+        let btnX =  div.querySelector("button");
+        btnX.addEventListener ("click",function(){
+        delsingleTask(this.id);
+        }.bind(this))
+       }
+} 
